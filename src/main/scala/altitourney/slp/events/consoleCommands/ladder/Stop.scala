0e297959bb2d1b769929a401d69e7d6319ed8e1e
Karl Hardenstine <khardenstine@gmail.com>
2013-09-23 23:28:09 -0400

    Ladder Stop logs server status

--- src/main/scala/altitourney/slp/events/consoleCommands/ladder/Stop.scala
+++ src/main/scala/altitourney/slp/events/consoleCommands/ladder/Stop.scala
@@ -8,8 +8,9 @@ import play.api.libs.json.JsValue
 class Stop(jsVal: JsValue) extends EventHandler(jsVal) {
 	getServerContext.getGame match {
 		case g: Ladder => {
-			getCommandExecutor.changeMap(getServerContext.getLobbyMap)
+			getCommandExecutor.logServerStatus()
 			getCommandExecutor.stopTournament()
+			getCommandExecutor.changeMap(getServerContext.getLobbyMap)
 		}
 		case _ => throw new ServerMessageException("Ladder is not currently running.")
 	}
